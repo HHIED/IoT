199f43f0d9d1496fbe2dcd12add8762fca3a41ed
SimonFlensted <simon_flensted@live.dk>
2018-04-06 10:47:57 +0200

    YES!!!

---
+++ IoTProject2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace IoTProject2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ IoTProject2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IoTProject2.WebApiApplication" Language="C#" %>
